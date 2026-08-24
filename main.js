fa0f793dbdbc66f6f0ea0876c1aa42d564ca7ba8
Ivan Isayenko <ivanisayenko@gmail.com>
2026-08-24 15:51:49 +0400

    feat: nesting-structure-comparasion

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-import { RomanNumerals } from "./katas/rank-4/roman-numerals.js";
+Array.prototype.sameStructureAs = function (other) {
+  // Return 'true' if and only if 'other' has the same
+  // nesting structure as 'this'.
+  //
+  // Note: You are given a function isArray(o) that returns
+  // whether its argument is an array.
+  const original = this;
 
-console.log(RomanNumerals.fromRoman("MMVIII"));
+  if (original.length !== other.length) return false;
+
+  let result = true;
+
+  for (let i = 0; i < original.length; i++) {
+    if (
+      (!Array.isArray(original[i]) && Array.isArray(other[i])) ||
+      (Array.isArray(original[i]) && !Array.isArray(other[i]))
+    ) {
+      return false;
+    } else if (Array.isArray(other[i]) && Array.isArray(original[i])) {
+      result = original[i].sameStructureAs(other[i]);
+    }
+  }
+
+  return result;
+};
+
+console.log([[[], []]].sameStructureAs([[1, 1]]));
